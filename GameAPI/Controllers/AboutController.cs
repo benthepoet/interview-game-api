35e31c7a08735081d7749d5d9a16cf3bf49e2d47
Ben Hanna <benpaulhanna@gmail.com>
2021-01-20 13:26:10 -0500

    Added about controller and fixed launch URL.

---
+++ GameAPI/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AboutController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Message = "Welcome to the Interview Game API!"
+            });
+        }
+    }
+}
